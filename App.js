0ec8a9bba31ae7a31353e7225610773261259c6d
vi5a7 <udamvisal18@kit.edu.kh>
2021-05-07 23:44:20 +0700

    Search destination screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
 import feed from './assets/data/feed'
 import SearchResultsScreen from './src/screens/SearchResults';
+import DestinationSearchScreen from './src/screens/DestinationSearch';
 
 const post1 = feed[0]
 
@@ -12,7 +13,8 @@ export default function App() {
   return (
     <SafeAreaView>
       {/* <HomeScreen/> */}
-      <SearchResultsScreen/>
+      {/* <SearchResultsScreen/> */}
+      <DestinationSearchScreen/>
     </SafeAreaView>
   );
 }
---
+++ src/screens/DestinationSearch/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 20,
+    },
+
+    textInput: {
+        fontSize: 20,
+        marginBottom: 20,
+    },
+
+    iconContainer: {
+        backgroundColor: '#e7e7e7',
+        padding: 7,
+        borderRadius: 10,
+        marginRight: 15,
+        marginBottom: 10
+    },
+
+    row:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 10,
+        borderBottomWidth: 1,
+        borderColor: 'lightgrey'
+    },
+
+    locationText: {
+
+    }
+})
+
+export default styles
